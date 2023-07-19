044bc11a2014263998d87d0dee83e3885213d75a
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-07-18 21:15:25 -0300

    docs: module doc added.

--- lib/banana_bank/users.ex
+++ lib/banana_bank/users.ex
@@ -1,4 +1,8 @@
 defmodule BananaBank.Users do
+  @moduledoc """
+  Provides delegations for user-related modules and functions.
+  """
+
   alias BananaBank.Users.Create
 
   defdelegate create(params), to: Create, as: :call

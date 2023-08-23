d480b47bfe09330704c5d1370fef83e19a793531
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-08-23 11:49:53 -0300

    fix: exception thrown when a non-existing userid was used to create the account.

--- lib/banana_bank/accounts/create.ex
+++ lib/banana_bank/accounts/create.ex
@@ -1,10 +1,13 @@
 defmodule BananaBank.Accounts.Create do
+  alias BananaBank.Users
   alias BananaBank.Accounts.Account
   alias BananaBank.Repo
 
-  def call(params) do
-    params
-    |> Account.changeset()
-    |> Repo.insert()
+  def call(%{"user_id" => id} = params) do
+    with {:ok, _user} <- Users.get(id) do
+      params
+      |> Account.changeset()
+      |> Repo.insert()
+    end
   end
 end

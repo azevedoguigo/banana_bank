bd35f320c6adf1b90b9581d3354ea4666bb17ab5
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-09-05 20:54:40 -0300

    chore: user auth function delegate.

--- lib/banana_bank/users.ex
+++ lib/banana_bank/users.ex
@@ -7,9 +7,11 @@ defmodule BananaBank.Users do
   alias BananaBank.Users.Get
   alias BananaBank.Users.Update
   alias BananaBank.Users.Delete
+  alias BananaBank.Users.Auth
 
   defdelegate create(params), to: Create, as: :call
   defdelegate get(id), to: Get, as: :call
   defdelegate update(params), to: Update, as: :call
   defdelegate delete(id), to: Delete, as: :call
+  defdelegate auth(params), to: Auth, as: :call
 end
